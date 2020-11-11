04f00ebb2c72a6a8b7c1706f6a699c1c8436e8ff
Johan Plasencia <plasenciajohan@hotmail.com>
2020-11-11 20:49:53 +0000

    update collisions

---
+++ Assets/Scripts/CollisionIgnorer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionIgnorer : MonoBehaviour
+{
+
+
+    [SerializeField] int layerToIgnore;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer== layerToIgnore)
+        {
+            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+        }
+    }
+}
